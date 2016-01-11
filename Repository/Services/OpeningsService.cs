32cc007e7f61b97d9a42522e976988b5db75a752
gumiz <micgrn@gmail.com>
2016-01-11 19:05:09 +0100

    openings completed with accounts

--- Repository/Services/OpeningsService.cs
+++ Repository/Services/OpeningsService.cs
@@ -9,24 +9,66 @@ namespace Repository.Services
 	public class OpeningsService : IOpeningsService
 	{
 		private readonly DefaultContext _dbContext;
+		private List<Opening> _openings;
+		private List<Opening> _result; 
+		private int _clientId;
+		private int _year;
+		private List<Account> _accounts;
 
 		public OpeningsService(DefaultContext dbContext)
 		{
-			this._dbContext = dbContext;
+			_dbContext = dbContext;
 		}
 
 		public IList<Opening> GetOpenings(int clientId, int year)
 		{
-			var dao = _dbContext.OpeningDao.Where(x => x.ClientId.Equals(clientId) && x.Year.Equals(year) ).ToList();
-			var result = Converter.ConvertList<OpeningDao, Opening>(dao);
-			return result;
+			_clientId = clientId;
+			_year = year;
+			GetAccounts();
+			GetOpenings();
+			AddNonExistantAccounts();
+			return _openings;
 		}
-
 		public void SaveOpenings(List<Opening> openings)
 		{
-			var openingsDao = Converter.ConvertList<Opening, OpeningDao>(openings);
+			_openings = openings;
+			if (_openings.Count == 0) return;
+			RemoveOldOpenings();
+			AddNewOpenings();
+		}
+
+		private void AddNonExistantAccounts()
+		{
+			foreach (var account in _accounts)
+				if (!_openings.Exists(x => x.Name.Equals(account.Name))) 
+					_openings.Add(new Opening {ClientId = _clientId, Year = _year, Name = account.Name, Description = account.Description, Ct = 0, Dt = 0});
+		}
+
+		private void GetAccounts()
+		{
+			var accountsDao = _dbContext.AccountDao.Where(c => c.ClientId.Equals(_clientId)).ToList();
+			_accounts = Converter.ConvertList<AccountDao, Account>(accountsDao);
+		}
+
+		private void GetOpenings()
+		{
+			var openingsDao = _dbContext.OpeningDao.Where(x => x.ClientId.Equals(_clientId) && x.Year.Equals(_year)).ToList();
+			_openings = Converter.ConvertList<OpeningDao, Opening>(openingsDao);
+		}
+
+		private void AddNewOpenings()
+		{
+			var openingsDao = Converter.ConvertList<Opening, OpeningDao>(_openings);
 			_dbContext.OpeningDao.AddRange(openingsDao);
 			_dbContext.SaveChanges();
+		}
+
+		private void RemoveOldOpenings()
+		{
+			var year = _openings[0].Year;
+			var clientId = _openings[0].ClientId;
+			var oldOpenings = _dbContext.OpeningDao.Where(c => c.ClientId.Equals(clientId) && c.Year.Equals(year));
+			_dbContext.OpeningDao.RemoveRange(oldOpenings);
 		}
 	}
 }
