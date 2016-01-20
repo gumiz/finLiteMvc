e05607767ceee5123fca74bbb101b533ac1bf49a
gumiz <micgrn@gmail.com>
2016-01-20 11:49:32 +0100

    reports completed

--- RepositoryTests/ReportsServiceTest.cs
+++ RepositoryTests/ReportsServiceTest.cs
@@ -1,7 +1,11 @@
-﻿using NSubstitute;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
 using NUnit.Framework;
 using Repository;
 using Repository.Abstract;
+using Repository.DAL;
 
 namespace RepositoryTests
 {
@@ -18,8 +22,14 @@ namespace RepositoryTests
 		[Test]
 		public void ConverterTest()
 		{
-			var result = _service.GetReports(2015, 1);
-			Assert.NotNull(result);
+			var docs = new List<DocumentDao> {new DocumentDao {AccountCt = "100"}, new DocumentDao {AccountCt = "200"}};
+			var result = LambdaTest(docs, d=>d.AccountCt);
+			Assert.AreEqual(1, result.Count);
+		}
+
+		private List<DocumentDao> LambdaTest(List<DocumentDao> documents, Func<DocumentDao, string> documentProperty)
+		{
+			return documents.Where(c=>documentProperty.Invoke(c).Equals("100")).ToList();
 		}
 	}
 }
